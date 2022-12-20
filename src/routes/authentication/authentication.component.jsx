d8249b32088f4e298e89a29e48d0bf80690e1ca4
Abraham <victor.abraham@charter.com>
2022-12-20 15:24:45 -0700

    Build Sign In page, sign in user firebase method

--- src/routes/authentication/authentication.component.jsx
+++ src/routes/authentication/authentication.component.jsx
@@ -1,8 +1,7 @@
 import { useEffect } from "react";
 import { getRedirectResult } from "firebase/auth";
 import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
-import Button from "../../components/button/button.component";
-import FormInput from "../../components/form-input/form-input.component";
+import SignInForm from "../../components/sign-in-form /sign-in-form.component";
 
 import {
   auth,
@@ -12,29 +11,10 @@ import {
 } from "../../utils/firebase/firebase.utils";
 
 const Authentication = () => {
-  useEffect(async () => {
-    const response = await getRedirectResult(auth);
-    if (response) {
-      const userDocRef = await createUserDocumentFromAuth(response.user);
-      console.log(userDocRef);
-    }
-  }, []);
-
-  const logGoogleUser = async () => {
-    const { user } = await signInWithGooglePopup();
-    const userDocRef = await createUserDocumentFromAuth(user);
-  };
-  const logGoogleRedirectUser = async () => {
-    const { user } = await signInWithGoogleRedirect();
-  };
-
   return (
     <div>
       <h1>Sign In Page</h1>
-      <button onClick={logGoogleUser}>Sign in with Google Popup</button>
-      <button onClick={signInWithGoogleRedirect}>
-        Sign in with Google Redirect
-      </button>
+      <SignInForm />
       <SignUpForm />
     </div>
   );
